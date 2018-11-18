557f1199226ea9bd78b485198285fc5bd007991d
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-11-18 19:00:45 +0600

    inventory sys up..

---
+++ themeleaflayouts/src/main/java/co/coderbd/themeleaflayouts/ThemeleaflayoutsApplication.java
@@ -0,0 +1,12 @@
+package co.coderbd.themeleaflayouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemeleaflayoutsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThemeleaflayoutsApplication.class, args);
+    }
+}

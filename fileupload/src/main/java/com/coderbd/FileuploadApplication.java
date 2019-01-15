293b1fa281fc03d3baa36e259c0b86000aaa781a
Mohammad Rajaul Islam <springapidev@gmail.com>
2019-01-15 09:04:31 +0600

    spring boot 2 book

---
+++ fileupload/src/main/java/com/coderbd/FileuploadApplication.java
@@ -0,0 +1,15 @@
+package com.coderbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileuploadApplication.class, args);
+    }
+//http://www.mkyong.com/spring-boot/spring-boot-file-upload-example/
+
+}
+

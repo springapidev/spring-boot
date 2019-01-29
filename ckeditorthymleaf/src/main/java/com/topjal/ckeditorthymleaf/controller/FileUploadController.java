d03d18f1435ea595f6ad885bcd6b5410051478c6
Mohammad Rajaul Islam <springapidev@gmail.com>
2019-01-29 21:30:10 +0600

    ck editor

--- ckeditorthymleaf/src/main/java/com/topjal/ckeditorthymleaf/controller/FileUploadController.java
+++ ckeditorthymleaf/src/main/java/com/topjal/ckeditorthymleaf/controller/FileUploadController.java
@@ -1,5 +1,6 @@
 package com.topjal.ckeditorthymleaf.controller;
 
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Base64;
@@ -12,17 +13,13 @@ import com.topjal.ckeditorthymleaf.exception.StorageFileNotFoundException;
 import com.topjal.ckeditorthymleaf.repo.ImageRepository;
 import com.topjal.ckeditorthymleaf.repo.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileUrlResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -100,6 +97,14 @@ public class FileUploadController {
    imageModel.get().getPic();
     System.out.println("I am called"+imageModel.get().getPic().toString());
     return "displaySingleFromDb";
+    }
+
+
+    public String ckFileSaveToDatabase(@RequestPart MultipartFile upload){
+String sourceName=upload.getOriginalFilename();
+
+
+        return "";
     }
 
 }

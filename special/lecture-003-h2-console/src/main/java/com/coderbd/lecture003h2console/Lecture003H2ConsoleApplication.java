13db0897bba52baa2e957e982f5540642e9d6c25
Mohammad Rajaul Islam <springapidev@gmail.com>
2019-07-09 02:18:15 +0600

    clean up repository.

---
+++ special/lecture-003-h2-console/src/main/java/com/coderbd/lecture003h2console/Lecture003H2ConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.coderbd.lecture003h2console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture003H2ConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lecture003H2ConsoleApplication.class, args);
+    }
+
+}
---
+++ special/lecture-003-h2-console/src/test/java/com/coderbd/lecture003h2console/Lecture003H2ConsoleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coderbd.lecture003h2console;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lecture003H2ConsoleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
